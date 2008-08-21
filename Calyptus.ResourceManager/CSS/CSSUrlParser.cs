edda731b897eabce5cf81fdcfd471563f7b6cdfd
Leon <Leon@b5b2b727-340c-3543-94ba-efdc1a8a0b4a>
2008-08-21 08:26:02 +0000

    Fix: when url(../file.jpg) not found, returns original url() path
    git-svn-id: http://clientside.svn.calyptus.se/ServerSideHelpers@231 b5b2b727-340c-3543-94ba-efdc1a8a0b4a

--- Calyptus.ResourceManager/CSS/CSSUrlParser.cs
+++ Calyptus.ResourceManager/CSS/CSSUrlParser.cs
@@ -8,7 +8,7 @@ namespace Calyptus.ResourceManager
 	internal static class CSSUrlParser
 	{
 		private static Regex _urlParser = new Regex("(?<=[\\:\\s]url\\()\\s*[\\'\\\"]?((?<=\\\")[^\\\"]*(?=\\\")|(?<=\\')[^\\']*(?=\\')|[^\\s\\'\\)\\,]+)[\\'\\\"]?\\s*(?:,\\s*[\\'\\\"]?((?<=\\\")[^\\\"]*(?=\\\")|(?<=\\')[^\\']*(?=\\')|[^\\s\\'\\)\\,]+)[\\'\\\"]?)?\\s*(?=\\))", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline);
-
+		
 		public static string ConvertUrls(string css, IResourceLocation baseLocation, IResourceURLFactory urlFactory, IImageResource[] includedImages)
 		{
 			return _urlParser.Replace(css, m =>
@@ -16,6 +16,7 @@ namespace Calyptus.ResourceManager
 				string assembly = m.Groups[2].Success ? m.Groups[1].Value : null;
 				string filename = m.Groups[2].Success ? m.Groups[2].Value : m.Groups[1].Value;
 				IResourceLocation location = ResourceLocations.GetLocation(baseLocation, assembly, filename);
+				if (location == null) return m.Groups[0].Value;
 				if (includedImages != null)
 					foreach (IImageResource res in includedImages)
 						if (location.Equals(res.Location))
