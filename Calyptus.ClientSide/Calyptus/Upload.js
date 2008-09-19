c9e35ca43c0be68e45060e4e68eb270f1a7e9e67
Leon <Leon@b5b2b727-340c-3543-94ba-efdc1a8a0b4a>
2008-09-19 08:06:28 +0000

    Added SwiffClass and Upload to logicalnames
    git-svn-id: http://clientside.svn.calyptus.se/ServerSideHelpers@290 b5b2b727-340c-3543-94ba-efdc1a8a0b4a

---
+++ Calyptus.ClientSide/Calyptus/Upload.js
@@ -0,0 +1,7 @@
+//@import MooTools.Core.js;
+//@import MooTools.Class.js;
+//@import MooTools.Class.Extras.js;
+//@import MooTools.Element.Dimensions.js;
+//@import MooTools.Utilities.Swiff.js;
+//@import Calyptus.Plugins.SwiffClass.js;
+//@include Calyptus.ClientSide.Lib.Calyptus.Request.Upload.js;
